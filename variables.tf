1175e21342cc6290c36e15021344821100fb6ac9
Anthony Rogic <anthony.rogic@valtech.com>
2022-01-09 15:22:29 +0100

    fixed formatting

--- variables.tf
+++ variables.tf
@@ -1,16 +1,16 @@
 variable "location" {
   description = "Azure Location"
-  default = "westeurope"
+  default     = "westeurope"
 }
 
 variable "resource_group_name" {
   description = "Azure Resource Group Name"
-  default = "test-rg"
+  default     = "test-rg"
 }
 
 variable "tags" {
   description = "Azure Resource Tags"
   default = {
-    environment = "development"
+    "environment" = "development"
   }
 }
