da7029595175cd7fa617b03cb94d73bffcc66f1d
Jovan Stojanov <jovan.stojanov@valtech.com>
2022-01-13 09:50:57 +0100

    Use env variables across all jobs

--- .github/scripts/tf-init.sh
+++ .github/scripts/tf-init.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 terraform init -input=false \
--backend-config "resource_group_name=$BACKEND_RESOURCE_GROUP" \
+-backend-config "resource_group_name=$BACKEND_RESOURCE_GROUP }}" \
 -backend-config "storage_account_name=$BACKEND_STORAGE_ACCOUNT_NAME" \
 -backend-config "container_name=$BACKEND_CONTAINER_NAME" \
 -backend-config "key=iac.tfstate"
