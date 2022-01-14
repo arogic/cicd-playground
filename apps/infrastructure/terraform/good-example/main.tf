1afd04f88b117a10987b4a4a80dd3067205318b3
Anthony Rogic <anthony.rogic@valtech.com>
2022-01-14 09:21:18 +0100

    fixing wrong module path

--- apps/infrastructure/terraform/good-example/main.tf
+++ apps/infrastructure/terraform/good-example/main.tf
@@ -8,14 +8,14 @@ terraform {
 }
 
 module "rg" {
-  source   = "./modules/arm_resource_group"
+  source   = "../modules/arm_resource_group"
   name     = var.resource_group_name
   location = var.location
   tags     = var.tags
 }
 
 module "vnet" {
-  source               = "./modules/arm_vnet"
+  source               = "../modules/arm_vnet"
   resource_group_name  = module.rg.name
   location             = module.rg.location
   virtual_network_name = var.virtual_network_name
