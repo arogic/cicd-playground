fb007c9ab81e04461f88ad25096fe031322373ec
anthony-rogic_valtech <anthony.rogic@valtech.com>
2022-10-10 11:24:21 +0200

    chore: test 2

--- .github/scripts/get-merge-commit.sh
+++ .github/scripts/get-merge-commit.sh
@@ -1,5 +1,11 @@
+git remote set-url origin https://x-access-token:${GITHUB_ACTIONS_TOKEN}@github.com/arogic/cicd-playground.git
 git config --global user.email "a.rogic@gmail.com"
-git config --global user.name "a.rogic"
+git config --global user.name "arogic"
+git checkout -b "${GIT_BRANCH}"
+echo "Test" >> test/README.md
+git add test/*
+git commit -m "chore: updated test readme"
+git push --set-upstream origin "${GIT_BRANCH}"
 GITHUB_TOKEN=$GITHUB_ACTIONS_TOKEN gh pr create --fill --base "${GIT_BASE_BRANCH}"
 GITHUB_TOKEN=$GITHUB_ACTIONS_TOKEN gh pr merge --rebase --auto
 GITHUB_TOKEN=$GITHUB_ACTIONS_TOKEN gh pr review --approve
