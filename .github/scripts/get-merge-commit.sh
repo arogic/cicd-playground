5fe0f87a104eaab92b2d3b68555d26eef1398659
anthony-rogic_valtech <anthony.rogic@valtech.com>
2022-10-13 10:17:25 +0200

    test-5

--- .github/scripts/get-merge-commit.sh
+++ .github/scripts/get-merge-commit.sh
@@ -16,7 +16,7 @@ PR_NUMBER=$(cat ./output.txt | sed 's/[^0-9]*//g')
 echo "PR_NUMBER: ${PR_NUMBER}"
 echo "Merging Pull Request"
 GITHUB_TOKEN=$GITHUB_ACTIONS_TOKEN gh pr merge --rebase --auto
-GITHUB_TOKEN=$GITHUB_ACTIONS_TOKEN gh pr review --approve
+# GITHUB_TOKEN=$GITHUB_ACTIONS_TOKEN gh pr review --approve
 GITHUB_TOKEN=$GITHUB_ACTIONS_TOKEN gh pr view "${GIT_BRANCH}" --json mergeCommit | jq -r .mergeCommit.oid > ./git-commit.txt
 ls -ltr ./git-commit.txt
 cat ./git-commit.txt
@@ -24,7 +24,7 @@ PR_MERGE_COMMIT=$(cat ./git-commit.txt)
 echo "PR_MERGE_COMMIT: ${PR_MERGE_COMMIT}"
 echo "VERSION: ${VERSION}"
 echo "SHOW COMMIT SHA"
-GITHUB_TOKEN=$GITHUB_ACTIONS_TOKEN gh pr list -S "${PR_NUMBER}" --json mergeCommit | jq -r '.[0].mergeCommit.oid'
+GITHUB_TOKEN=$GITHUB_ACTIONS_TOKEN gh pr list -s closed -S "${PR_NUMBER}" --json mergeCommit | jq -r '.[0].mergeCommit.oid'
 git pull
 git tag -a ${VERSION} -m "${VERSION}" ${PR_MERGE_COMMIT}
 git push --tags
