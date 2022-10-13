6c0b8c52988aada2fda2c32c682306a552280622
anthony-rogic_valtech <anthony.rogic@valtech.com>
2022-10-13 10:09:21 +0200

    test-2

--- .github/scripts/get-merge-commit.sh
+++ .github/scripts/get-merge-commit.sh
@@ -6,8 +6,14 @@ echo "Test" >> test/README.md
 git add test/*
 git commit -m "chore: updated test readme"
 git push --set-upstream origin "${GIT_BRANCH}"
+echo "Creating Pull Request"
 GITHUB_TOKEN=$GITHUB_ACTIONS_TOKEN gh pr create --fill --base "${GIT_BASE_BRANCH}" | tee -a ./output.txt
+echo "Output of Creating Pull Request"
 cat ./output.txt
+echo "Pull Request Number"
+cat ./output.txt | sed 's/[^0-9]*//g'
+PR_NUMBER=$(cat ./output.txt | sed 's/[^0-9]*//g')
+echo "Merging Pull Request"
 GITHUB_TOKEN=$GITHUB_ACTIONS_TOKEN gh pr merge --rebase --auto
 GITHUB_TOKEN=$GITHUB_ACTIONS_TOKEN gh pr review --approve
 GITHUB_TOKEN=$GITHUB_ACTIONS_TOKEN gh pr view "${GIT_BRANCH}" --json mergeCommit | jq -r .mergeCommit.oid > ./git-commit.txt
@@ -16,6 +22,8 @@ cat ./git-commit.txt
 PR_MERGE_COMMIT=$(cat ./git-commit.txt)
 echo "PR_MERGE_COMMIT: ${PR_MERGE_COMMIT}"
 echo "VERSION: ${VERSION}"
+echo "SHOW COMMIT SHA"
+GITHUB_TOKEN=$GITHUB_ACTIONS_TOKEN gh pr list -s closed -S "${PR_NUMBER}" --json mergeCommit | jq -r '.[0].mergeCommit.oid'
 git pull
 git tag -a ${VERSION} -m "${VERSION}" ${PR_MERGE_COMMIT}
 git push --tags
